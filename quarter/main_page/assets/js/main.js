aca614292a77d84a1c17547407a6b74fbed464e5
Abi Rivas <lourdes.2502@gmail.com>
2023-01-11 23:03:58 -0300

    Responsive design

--- quarter/main_page/assets/js/main.js
+++ quarter/main_page/assets/js/main.js
@@ -1,22 +1,39 @@
 function scrollHeader(){
-    const header = document.getElementById("header");
-    const logoHeader = document.getElementById("logoHeader");
+    const header = document.getElementById("header")
+    const logoHeader = document.getElementById("logoHeader")
     if  (this.scrollY >= 1) {
         header.classList.add("fixed-header")
-        logoHeader.style.display = "block";
+        logoHeader.style.display = "block"
     } else {
-        header.classList.remove("fixed-header");
-        logoHeader.style.display = "none";
+        header.classList.remove("fixed-header")
+        logoHeader.style.display = "none"
+    }
+
+    if (window.innerWidth <= 767) {
+        logoHeader.style.display = "block"
     }
 }
 
 window.addEventListener("scroll", scrollHeader)
 
 function adultVerification() {
-    const popup = document.getElementById("adultVerification");
-    popup.style.display = "none";
+    const popup = document.getElementById("adultVerification")
+    popup.style.display = "none"
 }
 
-function addToBag() {
-    const counter = document.getElementById("counter");
+// HAMBURGUER MENU
+const menuIcon = document.getElementById("hamMenuIcon"),
+    closeMenu = document.getElementById("closeMenu"),
+    hamMenuNav = document.getElementById("hamMenuNav")
+
+if  (menuIcon) {
+    menuIcon.addEventListener("click", () => {
+        hamMenuNav.style.display = "block"
+    })
+} 
+
+if(closeMenu){
+    closeMenu.addEventListener("click", () => {
+        hamMenuNav.style.display = "none"
+    })
 }
