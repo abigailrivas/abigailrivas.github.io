3550ab64725394d00455ad7a0457d7f82f46c35c
Abi Rivas <lourdes.2502@gmail.com>
2023-01-13 22:50:00 -0300

    Changed header and other sections

--- july/assets/js/base.js
+++ july/assets/js/base.js
@@ -1,10 +1,14 @@
 // ===== HEADER ======
 function scrollHeader(params) {
-    let header =  document.getElementById("header");
+    const header =  document.getElementById("header");
+    const logo = document.getElementById("logo");
     if (this.scrollY > 120) {
         header.classList.add("header-sticky");
+        logo.classList.add("logo--sticky");
+
     } else {
         header.classList.remove("header-sticky");
+        logo.classList.remove("logo--sticky");
     }
 }
 
