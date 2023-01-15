bd6dc4750f15b37c2ead7450bb95285924ef28b9
Abi Rivas <lourdes.2502@gmail.com>
2023-01-15 20:18:16 -0300

    I did the home section

--- july/assets/js/base.js
+++ july/assets/js/base.js
@@ -1,15 +1,26 @@
 // ===== HEADER ======
-function scrollHeader(params) {
-    const header =  document.getElementById("header");
-    const logo = document.getElementById("logo");
+function scrollHeader() {
+    const header =  document.getElementById("header")
+    const logo = document.getElementById("logo")
     if (this.scrollY > 120) {
-        header.classList.add("header-sticky");
-        logo.classList.add("logo--sticky");
+        header.classList.add("header-sticky")
+        logo.classList.add("logo--sticky")
 
     } else {
-        header.classList.remove("header-sticky");
-        logo.classList.remove("logo--sticky");
+        header.classList.remove("header-sticky")
+        logo.classList.remove("logo--sticky")
     }
 }
 
-window.addEventListener("scroll", scrollHeader);
+window.addEventListener("scroll", scrollHeader)
+
+// Hover on Shop july
+const shopJuly = document.getElementById("has-dropdown")
+const dropdownMenu = document.getElementById("dropdown-menu")
+shopJuly.addEventListener('mouseover', () => {
+    dropdownMenu.style.visibility = 'visible'
+})
+
+dropdownMenu.addEventListener('mouseleave', () => {
+    dropdownMenu.style.visibility = 'hidden'
+})
